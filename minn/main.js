85a25bc8dcad57c0ab777f1461d21e94cd61d952
aroberts2603 <aroberts2603@gmail.com>
2019-10-10 13:36:25 -0600

    overhauling the look of the website,  not quite done with it, in an intermmediate stage

---
+++ minn/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("scroll", function(e) {
+    checkScroll(window.scrollY);
+    parallax(window.scrollY);
+})
+
+function parallax(value) {
+    var para = (value*-0.17)-30;
+    document.getElementById("bg-code").style.marginTop = para+"px";
+}
+
+function checkScroll(value) {
+    if(value >= 300) {
+        console.log("hey");
+        document.getElementById("left-1").style.transform = "scale(1)";
+    }
+}
